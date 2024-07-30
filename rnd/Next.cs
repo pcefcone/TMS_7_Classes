cd1c21e065854aa7420f94210b36ce12a1c91a69
Yaroslav <yaroslav.kerez@gmail.com>
2024-07-30 21:41:49 +0200

    Add project files.

---
+++ rnd/Next.cs
@@ -0,0 +1,20 @@
+﻿
+namespace rnd
+{
+    internal class Next
+    {
+        private int v1;
+        private int v2;
+
+        public Next(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public static implicit operator int(Next v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
